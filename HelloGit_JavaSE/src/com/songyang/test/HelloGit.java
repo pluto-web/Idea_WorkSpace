06b7523716be160fdc6958d0a597a367746c344c
Mr.Song <424123579@qq.com>
2020-08-20 09:42:10 +0800

    IDEA提交

---
+++ HelloGit_JavaSE/src/com/songyang/test/HelloGit.java
@@ -0,0 +1,7 @@
+package com.songyang.test;
+
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("你好呀！洋");
+    }
+}
---
+++ HelloGit_JavaSE/src/com/songyang/test/Test2.java
@@ -0,0 +1,7 @@
+package com.songyang.test;
+
+public class Test2 {
+    public void Testtt(){
+        System.out.println("我好帅");
+    }
+}
